78069400314fdc1dd22a50bf0bf81c50590f1825
Marcos <MarcosJorgeBaez@gmail.com>
2024-09-19 10:39:48 -0400

    Demo página (b0.2)

--- index.php
+++ index.php
@@ -3,10 +3,23 @@
 <?php include_once 'head.php' ?>
 
 <body>
-    <div class="col-12 bg-primary" id="header">
-        <h3>Header</h3>
+    <div class="col-12 bg-black text-white d-flex justify-content-between" id="header">
+    <h4>Logo</h4>    
+    <h3>Pasitos de &aacute;ngel</h3>
+    <h4>Opciones</h4>
     </div>
-
+    <div class="col-12 bg-secondary" id="slider">
+        <label class="text-center w-100" style="height: 20em;" for="">Slider</label>
+    </div>
+    <div class="container bg-body-tertiary col-12">
+        <div class="w-100">
+            <h3>Contenido</h3>
+            <p>informacion informacion informacion informacion informacion informacion informacion informacion informacion informacion informacion informacion informacion informacion</p>
+        </div>
+    </div>
+    <div class="col-12 bg-black text-white d-flex justify-content-center" style="position: absolute; bottom:0;" id="footer">
+        <h3>Footer</h3>
+    </div>
 </body>
 
 </html>
